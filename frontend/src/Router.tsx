0d2ba376f36ec16e9f650f2520465ec765522091
bae0203 <gmbae06@dgsw.hs.kr>
2023-06-22 01:32:00 +0900

    :sparkles: [Header] Add Header

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
+import Header from "./components/common/Header";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
