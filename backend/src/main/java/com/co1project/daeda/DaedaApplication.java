754ca63d4f52a22b649c8bae90682e5f0786ed0e
jiseok419 <pyosik0419@gmail.com>
2023-06-21 22:43:48 +0900

    :memo: :: 백엔드 패키지 생성

---
+++ backend/src/main/java/com/co1project/daeda/DaedaApplication.java
@@ -0,0 +1,13 @@
+package com.co1project.daeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaedaApplication.class, args);
+	}
+
+}
