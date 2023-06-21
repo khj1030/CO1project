62e58ff2a52056d327de638fc0664ffdce901d88
bae0203 <gmbae06@dgsw.hs.kr>
2023-06-22 02:49:38 +0900

    :sparkles: [Mentoring] Add Modal

---
+++ frontend/src/styles/Form.style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.2);
+  width: 100%;
+  height: 110%;
+  margin-top: -5rem;
+`;
+
+export const FormBody = styled.nav`
+  width: 70%;
+  margin: 5rem auto;
+`;
