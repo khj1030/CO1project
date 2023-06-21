0fbb04054cc4b159c84c5d30e8cf50eb273ab8cd
bae0203 <gmbae06@dgsw.hs.kr>
2023-06-22 05:13:15 +0900

    ADD :: EXP Box

---
+++ frontend/src/components/experience/Experience.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Title from "../common/form/Title";
+import * as S from "../../styles/Form.style";
+import ContentsBox from "./contentsBox/ContentsBox";
+
+const Experience = () => {
+  const [array, setArray] = useState<string[]>([
+    "d",
+    "d",
+    "d",
+    "",
+    "d",
+    "",
+    "d",
+    "d",
+    "",
+    "d",
+    "",
+    "",
+    "",
+    "d",
+    "",
+    "d",
+    "d",
+    "d",
+    "d",
+    "d",
+    "",
+    "d",
+  ]);
+  return (
+    <S.FormBody>
+      <Title
+        title="경험 공유"
+        context="최고의 경험, 다양한 실패, 모두 이곳에서"
+      />
+      <S.ListWrap>
+        {array.map((v, i) => (
+          <ContentsBox isInPhoto={v !== ""} />
+        ))}
+      </S.ListWrap>
+    </S.FormBody>
+  );
+};
+
+export default Experience;
