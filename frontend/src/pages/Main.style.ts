8c5b9cc1626e631eb7fd9d10ba9560ceb228d627
sueeeeh <suehyun51@gmail.com>
2023-06-22 07:48:13 +0900

    :sparkles: [main] 퍼블리싱 완료

---
+++ frontend/src/pages/Main.style.ts
@@ -0,0 +1,200 @@
+import styled from "styled-components";
+
+export const Main1 = styled.div`
+    width: 100vw;
+    height: calc(100vh - 4.5rem);
+    background-color: aliceblue;
+
+    display: flex;
+    justify-content: center;
+`
+
+export const IntroBox1 = styled.div`
+    padding-top: 15vh;
+    width: 1285px;
+    height: 665px;
+
+    display: flex;
+    
+    box-shadow: 0px 8px 7px -5px rgba(0, 0, 0, 0.25);
+`
+
+export const IntroTextDiv1 = styled.div`
+    padding-left: 34px;
+    width: 465px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+`
+
+export const IntroText11 = styled.div`
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-size:3.5rem;
+`
+
+export const IntroText12 = styled.div`
+    font-family: 'Pretendard';
+    font-weight: 500;
+    font-size: 2.5rem;
+
+`
+
+export const IntroImg1 = styled.img`
+
+`
+//-----------------------------------------------
+export const Main2 = styled.div`
+    width: 100vw - 8vw;
+    height: 100vh;
+    background: #386CF4;
+    
+    display: flex;
+    align-items: center;
+`
+
+export const MainDiv2 = styled.div`
+    margin-left: 8vw;
+
+    width: 35vw;
+    min-width: 500px;
+`
+
+export const MainImg2 = styled.img`
+    width: 60vw;
+`
+
+export const MainText21 = styled.div`
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-size: 3.5rem;
+    color: #FFFFFF;
+`
+
+export const MainText22 = styled.div`
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-size: 2rem;
+    color: #FFFFFF;
+`
+
+export const Shortcut2 = styled.button`
+    margin-top: 25vh;
+
+    width: 18.7rem;
+    height: 6.2rem;
+
+    background: #FFFFFF;
+    border-radius: 50px;
+    border: none;
+
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-size: 3rem;
+    color: #386CF4;
+`
+//--------------------------------------
+export const Main3 = styled.div`
+    margin-left: 10vw;
+
+    width: calc(100vw - 15vw);
+    height: 100vh;
+
+    display: flex;
+    align-items: center;
+`
+
+export const MainImg3 = styled.img`
+    width: 40vw;
+`
+
+export const MainText31 = styled.div`
+font-family: 'Pretendard';
+font-weight: 600;
+font-size: 3.5rem;
+`
+
+export const MainText32 = styled.div`
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-size: 2rem;
+`
+
+export const MainDiv3 = styled.div`
+    margin-left: 5rem;
+
+    width: 35vw;
+    min-width: 500px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+`
+
+export const Shortcut3 = styled.button`
+    margin-top: 25vh;
+
+    width: 18.7rem;
+    height: 6.2rem;
+
+    background: #386CF4;
+    border-radius: 50px;
+    border: none;
+
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-size: 3.2rem;
+    color: #FFFFFF;
+`
+// ---------------------------------------------
+export const Main4 = styled.div`
+    width: 100vw - 8vw;
+    height: 100vh;
+    background: #386CF4;
+    
+    display: flex;
+    align-items: center;
+`
+
+export const MainDiv4 = styled.div`
+    margin-left: 8vw;
+
+    width: 35vw;
+    min-width: 500px;
+`
+
+export const MainImg4 = styled.img`
+    width: 60vw;
+`
+
+export const MainText41 = styled.div`
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-size: 3.5rem;
+    color: #FFFFFF;
+`
+
+export const MainText42 = styled.div`
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-size: 2rem;
+    color: #FFFFFF;
+`
+
+export const Shortcut4 = styled.button`
+    margin-top: 25vh;
+
+    width: 18.7rem;
+    height: 6.2rem;
+
+    background: #FFFFFF;
+    border-radius: 50px;
+    border: none;
+
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-size: 3rem;
+    color: #386CF4;
+`
