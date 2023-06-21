85adcb9fb337df96b96be8948206b2dbeb39e6cf
jiseok419 <pyosik0419@gmail.com>
2023-06-22 07:59:28 +0900

    :zap: :: 인터셉터 추가

--- backend/src/main/java/com/co1project/daeda/global/config/WebMvcConfig.java
+++ backend/src/main/java/com/co1project/daeda/global/config/WebMvcConfig.java
@@ -1,9 +1,22 @@
 package com.co1project.daeda.global.config;
 
+import com.co1project.daeda.global.interceptor.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+    private final AuthInterceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor);
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
