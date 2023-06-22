d4867855efbace503228ad38920e3c175aef9bda
bae0203 <gmbae06@dgsw.hs.kr>
2023-06-23 05:18:04 +0900

    ADD :: Server Connect

--- frontend/src/components/invest/modal/Modal.tsx
+++ frontend/src/components/invest/modal/Modal.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import API from "../../../util/API";
+import { IInvestorValue } from "../../../types/IInvestorValue";
 
-const Modal = () => {
+const Modal = ({ investorId }: { investorId: number }) => {
+  const [investorValue, setInvestorValue] = useState<IInvestorValue>();
+
+  useEffect(() => {
+    API.get(`api/investor/${investorId}`)
+      .then((e) => {
+        console.log(e);
+        let copy: IInvestorValue = e.data;
+        setInvestorValue(copy);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [investorId]);
+
   return (
     <div>
-        <div></div>
-        <p>Title</p>
-        <div>Modal</div>
+      <div>
+        <p>{investorValue?.title}</p>
+        <div>목표 금액 : {investorValue?.totalPrice}</div>
+      </div>
+      <p>{investorValue?.user.nickname}</p>
+      <p>{investorValue?.introduction}</p>
+      <button>투자하기</button>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
