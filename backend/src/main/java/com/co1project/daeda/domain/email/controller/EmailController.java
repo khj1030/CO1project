14ffbe40e5b51ab86ea831fd980445d090d512aa
jiseok419 <pyosik0419@gmail.com>
2023-06-23 05:33:27 +0900

    :memo: :: 멘토링 신청 email 추가

--- backend/src/main/java/com/co1project/daeda/domain/email/controller/EmailController.java
+++ backend/src/main/java/com/co1project/daeda/domain/email/controller/EmailController.java
@@ -3,12 +3,11 @@ package com.co1project.daeda.domain.email.controller;
 import com.co1project.daeda.domain.email.controller.dto.EmailAuthRequestDto;
 import com.co1project.daeda.domain.email.controller.dto.EmailCodeRequest;
 import com.co1project.daeda.domain.email.service.EmailService;
+import com.co1project.daeda.domain.user.domain.User;
+import com.co1project.daeda.global.annotation.CheckToken;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
 
@@ -27,6 +26,12 @@ public class EmailController {
     @PostMapping("/check")
     public boolean emailCheck(@RequestBody EmailCodeRequest request) {
         return emailService.checks(request.getCode());
+    }
+
+    @CheckToken
+    @PostMapping("/mentoring")
+    public void sendMentoringEmail(@RequestBody EmailAuthRequestDto emailAuthRequestDto, @RequestAttribute User user) throws MessagingException, UnsupportedEncodingException {
+        emailService.sendMentoringEmail(emailAuthRequestDto.getEmail(), user);
     }
 
 }
