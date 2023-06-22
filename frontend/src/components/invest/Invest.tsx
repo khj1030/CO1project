16db262cdc618cd1d49f48d2db9548ba93c5fd90
bae0203 <gmbae06@dgsw.hs.kr>
2023-06-23 05:17:59 +0900

    FIX :: Investor Props

--- frontend/src/components/invest/Invest.tsx
+++ frontend/src/components/invest/Invest.tsx
@@ -1,72 +1,101 @@
-import {useEffect,useState} from "react"
+import { useEffect, useState } from "react";
 import API from "../../util/API";
 import { IServerUserValue } from "../../types/IServerUserValue";
 import Title from "../common/form/Title";
-import * as S from "../../styles/Form.style"
+import * as S from "../../styles/Form.style";
 import { useNavigate } from "react-router-dom";
 import ContentsBox from "./contentsBox/ContentsBox";
+import Modal from "./modal/Modal";
 
 type Content = {
-    title : string,
-    introduction : string,
-    nickname : string
-}
+  title: string;
+  introduction: string;
+  nickname: string;
+  investorId: number;
+};
 
-interface IServerGetValue{
-    introduction:string;
-    createDate:string;
-    investorId:number;
-    modifiedDate:string;
-    title: string;
-    totalPrice: any;
-    user:IServerUserValue
+interface IServerGetValue {
+  introduction: string;
+  createDate: string;
+  investorId: number;
+  modifiedDate: string;
+  title: string;
+  totalPrice: any;
+  user: IServerUserValue;
 }
-
 
 const Invest = () => {
-    const navigate = useNavigate();
-    //let Cnt = [1,2,3,4,5,6,7,8,9];
-    const [contentData, setContentData] = useState<Content[]>([])
-    const [isLodding, setIsLodding] = useState<boolean>(true);
+  const navigate = useNavigate();
+  //let Cnt = [1,2,3,4,5,6,7,8,9];
+  const [isModalActive, setIsModalActive] = useState<boolean>(false);
+  const [investorId, setInvestorId] = useState<number>(0);
+  const [contentData, setContentData] = useState<Content[]>([]);
+  const [isLodding, setIsLodding] = useState<boolean>(true);
 
-    useEffect(()=>{
-        API.get('/api/investor')
-        .then((res)=>{
-            console.log(res);
-            let serverData:IServerGetValue[] = res.data;
-            let copy:Content[] = [];
-            // eslint-disable-next-line array-callback-return
-            serverData.map((val)=>{
-                console.log(val);
-                copy.push({title:val.title,introduction:val.introduction,nickname:val.user.nickname})
-            })
-            setContentData([...copy]);
-            setIsLodding(false)
-        })
-        .catch((err)=>{
-            console.error(err)
-        })
+  useEffect(() => {
+    API.get("/api/investor")
+      .then((res) => {
+        console.log(res);
+        let serverData: IServerGetValue[] = res.data;
+        let copy: Content[] = [];
+        // eslint-disable-next-line array-callback-return
+        serverData.map((val) => {
+          console.log(val);
+          copy.push({
+            title: val.title,
+            introduction: val.introduction,
+            nickname: val.user.nickname,
+            investorId: val.investorId,
+          });
+        });
+        setContentData([...copy]);
+        setIsLodding(false);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
+  }, []);
 
-    if(isLodding){
-        return(<>
-            로딩중입니다.........
-        </>)
-    }
+  if (isLodding) {
+    return <>로딩중입니다.........</>;
+  }
 
-    return(
-        <S.FormBody>
-            <S.AddBtn onClick={() => navigate("/creatementoring")}>
-                <p>+</p>
-            </S.AddBtn>
-            <Title title="투자자 모집" context="내가 키우고 싶은 회사에 투자 하세요."/>
-            <S.ListWrap>
-            {contentData.map((val,idx)=> 
-            <ContentsBox key={idx} title={val.title} name={val.nickname} introduce={val.introduction} />) }
-            </S.ListWrap>
-        </S.FormBody>
-    )
-}
+  return (
+    <>
+      {isModalActive && (
+        <S.Background
+          onClick={() => {
+            setIsModalActive(false);
+            setInvestorId(0);
+          }}
+        />
+      )}
+      <S.FormBody>
+        <S.AddBtn onClick={() => navigate("/creatementoring")}>
+          <p>+</p>
+        </S.AddBtn>
+        {isModalActive && <Modal investorId={investorId} />}
+        <Title
+          title="투자자 모집"
+          context="내가 키우고 싶은 회사에 투자 하세요."
+        />
+        <S.ListWrap>
+          {contentData.map((val, idx) => (
+            <ContentsBox
+              key={idx}
+              title={val.title}
+              name={val.nickname}
+              introduce={val.introduction}
+              investorId={val.investorId}
+              setModalActiveState={setIsModalActive}
+              setModalState={setInvestorId}
+            />
+          ))}
+        </S.ListWrap>
+      </S.FormBody>
+    </>
+  );
+};
 
-export default Invest
+export default Invest;
--- frontend/src/components/invest/contentsBox/ContentsBox.tsx
+++ frontend/src/components/invest/contentsBox/ContentsBox.tsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import * as S from "./Invest.style"
+import React, { Dispatch, SetStateAction } from "react";
+import * as S from "./Invest.style";
 
-interface IInvestContentsValue{
-    title : string;
-    name : string;
-    introduce : string;
+interface IInvestContentsValue {
+  title: string;
+  name: string;
+  introduce: string;
+  investorId: number;
+  setModalActiveState: Dispatch<SetStateAction<boolean>>;
+  setModalState: Dispatch<SetStateAction<number>>;
 }
 
-const ContentsBox = (props:IInvestContentsValue) => {
+const ContentsBox = (props: IInvestContentsValue) => {
   return (
-    <S.ContentBox >
-        <S.ContentTitle>{props.title}</S.ContentTitle>
-        <S.Content>{props.introduce}</S.Content>
-        <S.Info>
-            <S.ContentName>{props.name}</S.ContentName>
-            <S.LeftDays>7일 남음</S.LeftDays>
-        </S.Info>
+    <S.ContentBox
+      onClick={() => {
+        props.setModalActiveState(true);
+        props.setModalState(props.investorId);
+      }}
+    >
+      <S.ContentTitle>{props.title}</S.ContentTitle>
+      <S.Content>{props.introduce}</S.Content>
+      <S.Info>
+        <S.ContentName>{props.name}</S.ContentName>
+        <S.LeftDays>7일 남음</S.LeftDays>
+      </S.Info>
     </S.ContentBox>
-  )
-}
+  );
+};
 
-export default ContentsBox
+export default ContentsBox;
