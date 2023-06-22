1235c71e70e754b741c72debc7c32b4c95c16fdd
bae0203 <gmbae06@dgsw.hs.kr>
2023-06-22 23:25:20 +0900

    Add :: Create Invest Page

---
+++ frontend/src/pages/CreateinvestPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CreateInvest from "../components/createInvest/CreateInvest";
+
+const CreateinvestPage = () => {
+  return <CreateInvest />;
+};
+
+export default CreateinvestPage;
